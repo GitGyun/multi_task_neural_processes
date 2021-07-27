7f7d00f70b22dfe93b6c4dfe81a5f7bc4e1c06a8
donggyun <kdgyun425@gmail.com>
2021-07-27 08:34:57 +0000

    local script chance

--- scripts/ablation1.sh
+++ scripts/ablation1.sh
@@ -1,3 +1,3 @@
-python main.py --model mtp --data synthetic -ndp -glo -ptf _ndp_glo
-python main.py --model mtp --data synthetic_noised -ndp -glo -ptf _ndp_glo
-python main.py --model mtp --data synthetic_tasknoised -ndp -glo -ptf _ndp_glo
+python main.py --model mtp --data synthetic --cnp_stc -ndp -glo -ptf _cnpstc_ndp_glo
+python main.py --model mtp --data synthetic_noised --cnp_stc -ndp -glo -ptf _cnpstc_ndp_glo
+python main.py --model mtp --data synthetic_tasknoised --cnp_stc -ndp -glo -ptf _cnpstc_ndp_glo
--- scripts/ablation3.sh
+++ scripts/ablation3.sh
@@ -1,3 +1,3 @@
-python main.py --model mtp --data synthetic -ndp -igl -ptf _ndp_igl
-python main.py --model mtp --data synthetic_noised -ndp -igl -ptf _ndp_igl
-python main.py --model mtp --data synthetic_tasknoised -ndp -igl -ptf _ndp_igl
+python main.py --model mtp --data synthetic --cnp_stc -ndp -igl -ptf _cnpstc_ndp_igl
+python main.py --model mtp --data synthetic_noised --cnp_stc -ndp -igl -ptf _cnpstc_ndp_igl
+python main.py --model mtp --data synthetic_tasknoised --cnp_stc -ndp -igl -ptf _cnpstc_ndp_igl
